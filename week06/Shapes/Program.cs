b1607ba4a0ed8d2d388a670a6d51c364398de4cb
Vitalina Sizova <sizovavitalina@gmail.com>
2025-02-10 11:44:11 -0700

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("green", 3);
+        Rectangle rectangle = new Rectangle("pink", 2, 4);
+        Circle circle = new Circle("yellow", 2);
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} {shape.GetArea()}");
+        }
     }
 }
